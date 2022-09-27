3522cf478df70afb53260c3681ff2fd6b82f4579
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-09-27 20:23:38 +0300

    Removed unused dependency

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -34,16 +34,6 @@ const Header = styled("header", {});
 const Content = styled("main", {});
 
 const App = ({ stories }: AppProps) => {
-    const iframeRef = useRef<HTMLIFrameElement>(null);
-
-    useEffect(() => {
-        if (iframeRef.current) {
-            // TODO Make handshake before sending messages
-            // iframeRef.current?.contentWindow?.postMessage("Hello message");
-            // <iframe ref={iframeRef} src={`/__nanostory_iframe?filename=${encodeURI("./" + stories[0])}`} />
-        }
-    }, []);
-
     return (
         <Root>
             <Logo>
